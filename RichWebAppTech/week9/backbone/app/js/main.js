62867aabecd323ef137ab715300892d0aab786ae
samcoffey <samcoffey82@gmail.com>
2014-11-13 23:42:36 +0000

    Completed week 9

--- RichWebAppTech/week9/backbone/app/js/main.js
+++ RichWebAppTech/week9/backbone/app/js/main.js
@@ -11,7 +11,8 @@ var AppRouter = Backbone.Router.extend({
 
 	routes : {
 		"nationalities" : "nationalities",
-		"nationalities/:id" : "nationalities"
+		"nationalities/:id" : "nationalities",
+		"nationalities/students/:id" : "studentsOfNationality"
 	},
 
 	nationalities : function(id) {
@@ -41,6 +42,22 @@ var AppRouter = Backbone.Router.extend({
 
 			}
 		});
+	},
+	
+	//Added by sc
+	studentsOfNationality : function(id) {
+		var studentsCollection = new StudentsCollection();
+		studentsCollection.fetch({
+			success : function() {
+				
+				var studentsOfNationalityView = new StudentsOfNationalityView({
+					collection : studentsCollection
+				});
+				studentsOfNationalityView.flush();
+				studentsOfNationalityView.render(id);
+			}
+		});
+		
 	}
 });
 
@@ -50,10 +67,22 @@ var baseUrl = "http://www.lucalongo.eu/courses/2014-2015/questionnaireDIT/app/in
 // Base Model
 var BaseModel = Backbone.Model.extend();
 
+//Student Model - Added by sc - Automatically adds Id
+var StudentModel = Backbone.Model.extend({
+	model : BaseModel,
+	urlRoot : this.baseUrl + "/students"
+});
+
 // Nationalites Collection
 var NationalitesCollection = Backbone.Collection.extend({
 	model : BaseModel,
 	url : this.baseUrl + "/nationalities"
+});
+
+//Students Collection - Added by sc
+var StudentsCollection = Backbone.Collection.extend({
+	model : BaseModel,
+	url : this.baseUrl + "/students"
 });
 
 // Nationalites List View (nationalites navigation)
@@ -109,6 +138,46 @@ var NationalityDetailView = Backbone.View.extend({
 		return this;
 	},
 
+	pieChartRender : function(model) {
+		// Render pie chart
+	}
+});
+
+//Student Detail View (student detail) - Added by SC
+var StudentsOfNationalityView = Backbone.View.extend({
+
+	$content : $("#content"),
+
+	template : _.template($("#studentsOfNationality-detail-template").html()),
+
+	render : function(idNationality) {
+		// this collections models conincides with the list of objects in the collection
+		var self = this;
+		var template = this.template;
+		_.each(this.collection.models, function(model) {
+			var studentID = model.toJSON().id;
+			
+			//Setting a student Model
+			var student = new StudentModel({
+				id : studentID
+			});
+			
+			//The fetch below will perform GET ../student/studentID
+			student.fetch({
+				success : function(s) {
+					if(s.toJSON().id_nationality == idNationality) {
+						self.$content.append(template(s.toJSON()));
+					}
+				}
+			});
+		});
+
+	},
+
+	flush : function() {
+		this.$content.empty(); // jquery method
+	},
+	
 	pieChartRender : function(model) {
 		// Render pie chart
 	}
